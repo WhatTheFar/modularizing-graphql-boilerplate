9f504837856424e40778776c5cb3bc42c446e9d6
BB-8 <ichannel.jakpat@gmail.com>
2018-10-29 21:49:18 +0700

    build(scripts): fix build prod scripts

--- scripts/build-src.sh
+++ scripts/build-src.sh
@@ -3,6 +3,8 @@ rm -rf dist
 ts-node prisma/datamodel.ts
 graphql codegen
 ts-node scripts/generateTypeDefs.ts
+tsc
+echo # Copying graphql schema
 mkdir -p ./dist/generated
 cp ./src/generated/schema.graphql ./dist/generated/schema.graphql
-tsc
+cp ./src/common/extend-type/extend-type.graphql ./dist/common/extend-type/extend-type.graphql
